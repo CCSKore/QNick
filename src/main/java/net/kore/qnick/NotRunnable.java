f60e3a871cc8bb9d3146dfbe011910bf80d0110b
Nova <100384210+CCSNova@users.noreply.github.com>
2023-10-22 15:11:43 +0100

    Add a GUI that shows when running jar, release

---
+++ src/main/java/net/kore/qnick/NotRunnable.java
@@ -0,0 +1,9 @@
+package net.kore.qnick;
+
+import javax.swing.JOptionPane;
+
+public class NotRunnable {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "This jar file is not runnable, please put it in your server's plugin folder to use QNick", "Not Runnable!", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
